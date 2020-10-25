13fafef203f37132f43de38720298a2eaee4686a
sam p <spoveda545@gmail.com>
2020-10-24 22:51:09 -0400

    base user context

--- src/App.js
+++ src/App.js
@@ -7,36 +7,33 @@ import  UserLoggedInContext from './UserLoggedInContext'
 import './App.css';
 
 class App extends React.Component {
+
   render() {
 		// <Route exact path='/sign-up' component={SignUp}/>
 		// <Route exact path='/login' component={Login}/>
-		return (
-			<UserLoggedInContext.Provider>
-			{({ loggedIn })=>{
-				return (
+			return (
+				<UserLoggedInContext.Provider>
 					<div className="App">
 						<header className="App-header">
-						{/* this header will changed based on if user is logged in with Context*/}
+							{/* this header will changed based on if user is logged in with Context*/}
 							<h1>
 								SpellBound
 							</h1>
-						<ul>
-							<li>
-								<a href='#'></a>
-							</li>
-							<li>
-								<a href="#">Sign Up</a>
-							</li>
-						</ul>
+							<ul>
+								<li>
+									<a href='#'>Sign in</a>
+								</li>
+								<li>
+									<a href="#">Sign up +</a>
+								</li>
+							</ul>
 						</header>
 						<main>
 							<Route exact path='/' component={LandingPage}/>
 						</main>
 					</div>
-				);
-			}}
-			</UserLoggedInContext.Provider>
-		);
+				</UserLoggedInContext.Provider>
+			);
 	}
 }
 
