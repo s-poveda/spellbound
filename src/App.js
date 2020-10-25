3a9128fbaf7911d56a745129d707f7920e73b908
sam p <spoveda545@gmail.com>
2020-10-24 22:40:47 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import  React from 'react';
+import { Route } from 'react-router';
+import LandingPage from './Components/LandingPage/LandingPage';
+import  SignUp from './Components/SignUp/SignUp';
+import Login from './Components/Login/Login';
+import  UserLoggedInContext from './UserLoggedInContext'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+		// <Route exact path='/sign-up' component={SignUp}/>
+		// <Route exact path='/login' component={Login}/>
+		return (
+			<UserLoggedInContext.Provider>
+			{({ loggedIn })=>{
+				return (
+					<div className="App">
+						<header className="App-header">
+						{/* this header will changed based on if user is logged in with Context*/}
+							<h1>
+								SpellBound
+							</h1>
+						<ul>
+							<li>
+								<a href='#'></a>
+							</li>
+							<li>
+								<a href="#">Sign Up</a>
+							</li>
+						</ul>
+						</header>
+						<main>
+							<Route exact path='/' component={LandingPage}/>
+						</main>
+					</div>
+				);
+			}}
+			</UserLoggedInContext.Provider>
+		);
+	}
 }
 
 export default App;
