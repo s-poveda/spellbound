1b3cc3c8f3d81e796b9bda83eeb9d31bee92183a
sam p <spoveda545@gmail.com>
2020-11-10 17:56:02 -0500

    Login page first pass

--- src/Components/LoginPage/LoginPage.js
+++ src/Components/LoginPage/LoginPage.js
@@ -1,19 +1,67 @@
 import React, { Component } from 'react';
+import authService from './../../services/authService';
+import tokenService from './../../services/tokenService';
 import './loginpage.css';
 
 export default class LoginPage extends Component {
-  render() {
-    return (
-      <div>
-				<h2>Log in</h2>
-				<form id="login-form">
+	state = {
+		username: {
+			touched: false,
+			value: '',
+		},
+		password: {
+			touched: false,
+			value: '',
+		},
+		error: null,
+	};
 
-					<input aria-label='Username' name='username' placeholder='Username' type='text'/>
-					<input aria-label='Password' name='password' placeholder='Password'/>
+	onInputChange = ev => {
+		const newState = Object.assign({}, this.state);
+		newState[ev.target.name].value = ev.target.value;
+		newState[ev.target.name].touched = true;
+		this.setState(newState);
+	};
+
+	onLoginSubmit= (ev) => {
+		ev.preventDefault();
+		const { username, password } = this.state;
+		authService.login(username.value, password.value)
+		.then( obj => {
+			console.log(obj);
+			tokenService.saveAuthToken(obj.authToken);
+			this.props.history.goBack();
+		})
+		.catch( error=> {
+			console.log(error);
+			this.setState({ error });
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<h2>Log in</h2>
+				<form id='login-form' onSubmit={this.onLoginSubmit}>
+					<input
+						aria-label='Username'
+						value={this.state.username.value}
+						name='username'
+						placeholder='Username'
+						onChange={this.onInputChange}
+						type='text'
+					/>
+					<input
+						aria-label='Password'
+						value={this.state.password.value}
+						name='password'
+						placeholder='Password'
+						onChange={this.onInputChange}
+						type='password'
+					/>
 					<button>Log in</button>
-					
 				</form>
 			</div>
-    );
-  }
-};
+		);
+	}
+}
