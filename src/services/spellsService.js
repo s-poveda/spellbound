9854c2d627a469b463f68bfa4aa33885428f8f3f
sam p <spoveda545@gmail.com>
2020-11-11 17:52:58 -0500

    const assigned spells service

--- src/services/spellsService.js
+++ src/services/spellsService.js
@@ -1,67 +1,26 @@
-import { API_URL } from './config';
+import { API_URL, fetchHandler } from './config';
+const spellsService = {
 
-class ApiError extends Error {
-	constructor(message, code) {
-		super(message);
-		this.fromApi = true;
-	}
-}
-
-function fetchHandler(...args) {
-	return fetch(...args)
-		.then(res => {
-			if (!res.ok) {
-				throw new ApiError(res.message);
-			}
-			if (res.status === 204) return {};
-			return res.json();
-		})
-		.then(data => {
-			return data;
-		})
-		.catch(e => {
-			throw new ApiError(e.message);
+	getAllSpells(offset = 0) {
+		return fetchHandler(`${API_URL}/spells?offset=${offset}`);
+	},
+	getSpellById(id) {
+		return fetchHandler(`${API_URL}/spells/${id}`);
+	},
+	postSpell(spell, token) {
+		const body = JSON.stringify({
+			title: spell.title,
+			description: spell.description,
 		});
-}
-
-function getAllSpells(offset = 0) {
-	return fetchHandler(`${API_URL}/spells?offset=${offset}`);
-}
-
-function getSpellById(id) {
-	return fetchHandler(`${API_URL}/spells/${id}`);
-}
-
-function getSpellsByUsername(username, page = 1, spellsPerPage = 10) {
-	spellsPerPage = Number(spellsPerPage);
-	page = Number(page);
-	if (!spellsPerPage || !page) {
-		throw new Error('page and spellsPerPage must be numbers');
+		return fetchHandler(`${API_URL}/spells`, {
+			method: 'POST',
+			headers: {
+				Authorization: `Bearer ${token}`,
+				'Content-Type': 'application/json',
+			},
+			body,
+		});
 	}
-	fetchHandler(
-		`${API_URL}/users/${username}?limit=${spellsPerPage}&offset=${spellsPerPage *
-			(page - 1)}`
-	);
-}
-
-function postSpell(spell, token) {
-	const body = JSON.stringify({
-		title: spell.title,
-		description: spell.description,
-	});
-	return fetchHandler(`${API_URL}/spells`, {
-		method: 'POST',
-		headers: {
-			Authorization: `Bearer ${token}`,
-			'Content-Type': 'application/json',
-		},
-		body,
-	});
 }
 // eslint-disable-next-line
-export default {
-	getAllSpells,
-	getSpellById,
-	getSpellsByUsername,
-	postSpell,
-};
+export default spellsService;
