a162109aa05ed2403efa444e7ddc17b46789bf30
sam p <spoveda545@gmail.com>
2020-11-11 17:53:48 -0500

    fetch handler is now named export in config

--- src/services/config.js
+++ src/services/config.js
@@ -1,2 +1,26 @@
 export const API_URL = 'http://localhost:8000/api';
 export const TOKEN_KEY = 'spellbound-api-token';
+
+class ApiError extends Error {
+	constructor(message, code) {
+		super(message);
+		this.fromApi = true;
+	}
+}
+
+export function fetchHandler(...args) {
+	return fetch(...args)
+		.then(res => {
+			if (!res.ok) {
+				throw new ApiError(res.message);
+			}
+			if (res.status === 204) return {};
+			return res.json();
+		})
+		.then(data => {
+			return data;
+		})
+		.catch(e => {
+			throw new ApiError(e.message);
+		});
+}
