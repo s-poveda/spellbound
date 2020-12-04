c7c276e52bfad2f7f95e38e0b2a2271996c07622
sam p <spoveda545@gmail.com>
2020-12-03 21:30:23 -0500

    Landing page message changed

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -8,25 +8,15 @@ export  default class LandingPage extends Component {
 	render() {
 		return (
 				<ApiContext.Consumer>
-				{ ({spells}) => {
+				{ ({spells, loggedIn}) => {
 				//each spell list return a <section>
 				const { allSpells } = spells;
 				return (
 					<Fragment>
 					<p className='landing-page-description'>
 						Your one stop for creating and sharing custom spell ideas for any game!
-						<br/>
-						<br/>
-						Want to make your own spell ideas? Sign up!
 					</p>
-					<aside>
-					<p>
-					<strong>Demo User:</strong> <em>Demo User</em>
-					</p>
-					<p>
-					<strong>Demo Password:</strong> <em>DemoPassword1!</em>
-					</p>
-					</aside>
+
 					<div className='main-content'>
 						<SpellList spells={allSpells} title='Recently Added'/>
 					</div>
